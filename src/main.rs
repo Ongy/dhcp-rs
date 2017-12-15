52285517a9f6dff43f778d31d7603e34f41936b6
Markus Ongyerth <ongy@ongy.net>
2017-12-15 07:29:07 +0100

    Hide dropping caps behind feature, in case it's not supported

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,8 @@ extern crate time;
 extern crate ipnetwork;
 
 extern crate privdrop;
+
+#[cfg(feature="dropcaps")]
 extern crate caps;
 
 mod frame;
@@ -34,6 +36,7 @@ use clap::{Arg, App};
 use std::str::FromStr;
 
 // This asumes linux! are there proper compile macros for this?
+#[cfg(feature="dropcaps")]
 fn drop_caps() -> std::io::Result<()> {
     info!("Droping capabilities");
     let pid = i32::from_str(std::fs::read_link("/proc/self")?.to_str().unwrap()).unwrap();
@@ -68,10 +71,15 @@ fn drop_caps() -> std::io::Result<()> {
     Ok(())
 }
 
+#[cfg(not(feature = "dropcaps"))]
+fn drop_caps() -> Result<(), String> {
+    Err(String::from("This version was compiled without support for capabilities"))
+}
+
 // We could let the threads drop their own capabilities, at the right moment, but changing user
 // will be per-process, not per-thread. So we first try that, then do a funny hack if that failed.
 fn drop_user() {
-    match privdrop::PrivDrop::default().user("dhp").apply() {
+    match privdrop::PrivDrop::default().user("dhcp").apply() {
         Ok(()) => {},
         Err(e) => {
             warn!("Running as root");
@@ -127,7 +135,7 @@ fn main() {
                  .takes_value(true))
             .get_matches();
 
-    let path = matches.value_of("dhcpd").unwrap_or("/etc/dhcp/dhcpd.conf");
+    let path = matches.value_of("config").unwrap_or("/etc/dhcp/dhcpd.conf");
 
     run_server(path);
 }
